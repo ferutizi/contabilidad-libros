6b8122a3c7c3b69516d8ba6e047d4772bc67f0d1
ferutizi <fer.utizi@gmail.com>
2024-03-07 19:11:15 -0300

    table styles

--- src/app/components/Tabla.tsx
+++ src/app/components/Tabla.tsx
@@ -6,40 +6,46 @@ interface TablaProps {
 
 export default function tabla({tabla}: TablaProps ) {
   return(
-    <table className="w-full border">
-    <thead>
-      <tr>
-        <td>Fecha</td>
-        <td>Nº de Asiento</td>
-        <td>Detalle</td>
-        <td>Cuenta</td>
-        <td>Debe</td>
-        <td>Haber</td>
-      </tr>
-    </thead>
-    <tbody>
-      {
-        tabla.map(e => 
-          <>
-            <tr className="text-white">
-              <td>{e.fecha}</td>
-              <td>{e.id}</td>
-              <td>{e.detalle}</td>
-            </tr>
-              {e.registros.map(e => (
-                <tr className="text-white">
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td>{e.cuenta}</td>
-                  <td>{e.debe}</td>
-                  <td>{e.haber}</td>
-                </tr>
-              ))}
-        </> 
-        )
-      }
-    </tbody>
-  </table>
+    <table className="w-full border text-white">
+      <thead className="border border-red-800 text-center">
+        <tr>
+          <td className="w-24 px-12">Fecha</td>
+          <td className="w-12 px-12">Nº de Asiento</td>
+          <td className="w-60 px-12">Detalle</td>
+          <td className=" px-12">Cuenta</td>
+          <td className=" px-12">Debe</td>
+          <td className=" px-12">Haber</td>
+        </tr>
+      </thead>
+      <tbody className="">
+        {
+          tabla.map(e => 
+            <>
+              <tr className="bg-stone-950">
+                <td>{e.fecha}</td>
+                <td className="text-center">{e.id}</td>
+                <td>{e.detalle}</td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+                {e.registros.map(e => (
+                  <tr className="border-b border-b-stone-900">
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td>{e.cuenta}</td>
+                    <td className="text-end border border-x-stone-900 border-y-0">$ {e.debe}</td>
+                    <td className="text-end">$ {e.haber}</td>
+                  </tr>
+                ))}
+              <tr className="flex justify-center items-center ">
+                <button className="bg-stone-900 w-full">Editar Asiento</button>
+              </tr>
+          </> 
+          )
+        }
+      </tbody>
+    </table>
   )
 }
